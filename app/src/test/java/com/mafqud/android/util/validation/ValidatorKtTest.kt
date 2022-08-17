cc939c093aa36f490fddc8171ab82818d242b56a
ahmed shehata <ahamdyshehata7@gmail.com>
2022-08-17 14:42:03 +0200

    add some RobolectricTestRunner

--- app/src/test/java/com/mafqud/android/util/validation/ValidatorKtTest.kt
+++ app/src/test/java/com/mafqud/android/util/validation/ValidatorKtTest.kt
@@ -2,9 +2,12 @@ package com.mafqud.android.util.validation
 
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
+@RunWith(RobolectricTestRunner::class)
 class ValidatorKtTest {
-
 
     @Test
     fun `isValidEmail, when passing a valid emails, then return true`() {
@@ -15,11 +18,24 @@ class ValidatorKtTest {
         emails.add("a@mafqud.com")
         emails.add("a@hotmail.com")
 
-
-
         emails.forEach { email ->
             val result = isValidEmail(email)
             assertThat(result).isTrue()
+        }
+    }
+
+    @Test
+    fun `isValidEmail, when passing a not valid emails, then return false`() {
+        val emails = mutableListOf<String>()
+        emails.add("a'$'gmail.com")
+        emails.add("a*yahoo.com")
+        emails.add("a@instcom")
+        emails.add("a45222.com")
+        emails.add("a@hotmail")
+
+        emails.forEach { email ->
+            val result = isValidEmail(email)
+            assertThat(result).isFalse()
         }
     }
 }
